0cd2f9d33c75302197ecb44a59ba125d56bd2795
Samuel Vicentini <samuelvicentini@outlook.com>
2020-12-09 10:28:32 -0300

    exercicio 0201 variaveis

---
+++ JavaScript Completo ES6+/Exercicios/variaveis/script.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+
+var nome = 'Samuel';
+
+// Declarar uma variável com a sua idade
+
+var idade = 20;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var comidaPreferida = 'Lasanha'
+
+// Atribuir valor a sua comida favorita
+
+var valorComidaPreferida = 19.99
+
+// Declarar 5 variáveis diferentes sem valores
+
+var sobrenome, cidade, estado, numero, logradouro;
