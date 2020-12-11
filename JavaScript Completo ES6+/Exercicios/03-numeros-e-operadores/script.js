fbc0b42e95e7f26b4a8270ee240402eebbc98917
Samuel Vicentini <samuelvicentini@outlook.com>
2020-12-11 13:39:56 -0300

    exercicio 0203 numeros e operacionais

---
+++ JavaScript Completo ES6+/Exercicios/03-numeros-e-operadores/script.js
@@ -0,0 +1,25 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; // = 35
+
+// Crie duas expressões que retornem NaN
+
+var valor = 15;
+var parcelas = '2x';
+var valorAPagar = valor / parcelas;
+
+// Somar a string '200' com o número 50 e retornar 250
+
+var soma = +'200' + 50;
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+var numero = 5
+var incremento = ++numero
+console.log(incremento)
+
+// Como dividir o peso por 2?
+var numero = +'80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2; // NaN (Not a Number)
+var dividirPorDois = numero / 2;
