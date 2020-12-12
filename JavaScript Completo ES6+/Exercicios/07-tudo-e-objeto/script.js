b24e438f19b90c58306f7141cf538b1fb07add0d
Samuel Vicentini <samuelvicentini@outlook.com>
2020-12-12 10:39:13 -0300

    exercicio 0207 tudo e objeto

---
+++ JavaScript Completo ES6+/Exercicios/07-tudo-e-objeto/script.js
@@ -0,0 +1,21 @@
+// nomeie 3 propriedades ou métodos de strings
+
+var nome = 'samuel';
+
+nome.replace()
+nome.split()
+nome.toLowerCase()
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+document.querySelector();
+document.write();
+document.inputEncoding
+document.isDefaultNamespace
+document.removeChild()
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+Clipboard.js
