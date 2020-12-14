d923d16df3192acc17e20fbf90301a002656e9b4
Samuel Vicentini <samuelvicentini@outlook.com>
2020-12-13 22:31:22 -0300

    exercicio 0210 escopo

---
+++ JavaScript Completo ES6+/Exercicios/10-escopo/script.js
@@ -0,0 +1,34 @@
+// Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+}
+console.log(var, marca, portas);
+
+//dentro de um objeto não pode definir variaveis ou coisa do genero, não termina com ;
+
+// Como corrigir o erro abaixo?
+function somarDois(x) {
+  const dois = 2;
+  return x + dois;
+}
+function dividirDois(x) {
+  return x + dois;
+}
+somarDois(4);
+dividirDois(6);
+
+// dividirDois não tem acesso a constante dois
+
+// O que fazer para total retornar 500?
+const numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
+
+//apenas mudar as constantes
