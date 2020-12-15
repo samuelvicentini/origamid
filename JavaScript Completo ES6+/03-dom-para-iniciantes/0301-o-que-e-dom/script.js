bed3c85a69f8644a25a57f10fdca1c03d6bc4c33
Samuel Vicentini <samuelvicentini@outlook.com>
2020-12-15 14:19:16 -0300

    exercicio 0301 o que e dom

---
+++ JavaScript Completo ES6+/03-dom-para-iniciantes/0301-o-que-e-dom/script.js
@@ -0,0 +1,16 @@
+// Retorne o url da página atual utilizando o objeto window
+
+console.log(window.location.href)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+console.log(document.querySelector('.titulo'))
+
+// Retorne a linguagem do navegador
+
+console.log(navigator.language)
+
+// Retorne a largura da janela 
+
+console.log(document.querySelector('.titulo').clientWidth)
