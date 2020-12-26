c201435423dae8767a94e82a0dda8cf261aaa5e2
Samuel Vicentini <samuelvicentini@outlook.com>
2020-12-25 21:25:49 -0300

    criando animacoes aos dt e dd

--- JavaScript Completo ES6+/03-dom-para-iniciantes/0310-accordion-list/script.js
+++ JavaScript Completo ES6+/03-dom-para-iniciantes/0310-accordion-list/script.js
@@ -1,20 +1,42 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabMenu.length && tabContent.length) {
-  //Sempre vir com a class ativo
-  tabContent[0].classList.add('ativo');
+  if(tabMenu.length && tabContent.length) {
+    //Sempre vir com a class ativo
+    tabContent[0].classList.add('ativo');
 
-  function activeTab(index) {
-    tabContent.forEach(content => {
-      content.classList.remove('ativo');
-    });
-    tabContent[index].classList.add('ativo');
-  }
+    function activeTab(index) {
+      tabContent.forEach(content => {
+        content.classList.remove('ativo');
+      });
+      tabContent[index].classList.add('ativo');
+    }
 
-  tabMenu.forEach((item, index) => {
-    item.addEventListener('click', () => {
-      activeTab(index);
+    tabMenu.forEach((item, index) => {
+      item.addEventListener('click', () => {
+        activeTab(index);
+      })
     })
-  })
+  }
 }
+initTabNav();
+
+function initAccordion() {
+  const accordionListe = document.querySelectorAll('.js-accordion dt');
+
+  if(accordionListe.length) {
+    accordionListe[0].classList.add('ativo');
+    accordionListe[0].nextElementSibling.classList.toggle('ativo')
+  
+    function actveAccordion() {
+      this.classList.toggle('ativo')
+      this.nextElementSibling.classList.toggle('ativo')
+    }
+  
+    accordionListe.forEach(item => {
+      item.addEventListener('click', actveAccordion)
+    })
+  }
+}
+initAccordion();
