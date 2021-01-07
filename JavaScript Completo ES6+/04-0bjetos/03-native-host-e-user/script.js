787413999bbc4f318d25e46742569c8602b22ae8
Samuel Vicentini <samuelvicentini@outlook.com>
2021-01-07 10:05:37 -0300

    exercicio 0403 native host e user

---
+++ JavaScript Completo ES6+/04-0bjetos/03-native-host-e-user/script.js
@@ -0,0 +1,18 @@
+// Liste 5 objetos nativos
+
+-- Array
+-- Object
+-- String
+-- Function
+-- Number
+
+// Liste 5 objetos do browser
+
+-- NodeList
+-- HTMLCollection
+-- Document
+-- HTMLAnchorElement
+-- HTMLAreaElement
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
