2545ea8e25253398cc368f70574e0b96c716c26a
Samuel Vicentini <samuelvicentini@outlook.com>
2021-01-20 20:06:39 -0300

    criando modulos para o js

---
+++ JavaScript Completo ES6+/05-efeitos-no-DOM/0502-modules/js/script.js
@@ -0,0 +1,9 @@
+import initSmoothScroll from './modules/scroll-suave.js';
+import initAnimScroll from './modules/animacao-scroll.js';
+import initAccordionList from './modules/accordion.js';
+import initTabNav from './modules/tab-navigation.js';
+
+initSmoothScroll();
+initAnimScroll();
+initAccordionList();
+initTabNav();
