d89c580a555376b6f8e5b403b02d2240135dc23e
Samuel Vicentini <samuelvicentini@outlook.com>
2021-01-21 11:31:39 -0300

    arquivos para aula

---
+++ JavaScript Completo ES6+/05-efeitos-no-DOM/0504-tooltip/js/script.js
@@ -0,0 +1,13 @@
+import initSmoothScroll from './modules/scroll-suave.js';
+import initAnimScroll from './modules/animacao-scroll.js';
+import initAccordionList from './modules/accordion.js';
+import initTabNav from './modules/tab-navigation.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+
+initSmoothScroll();
+initAnimScroll();
+initAccordionList();
+initTabNav();
+initModal();
+initTooltip();
