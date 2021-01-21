b3aa8a10a70e53ced1e2ef0e9538cb0a49c542b7
Samuel Vicentini <samuelvicentini@outlook.com>
2021-01-21 11:28:48 -0300

    criando a logica do modal

--- JavaScript Completo ES6+/05-efeitos-no-DOM/0503-modal/js/script.js
+++ JavaScript Completo ES6+/05-efeitos-no-DOM/0503-modal/js/script.js
@@ -2,8 +2,10 @@ import initSmoothScroll from './modules/scroll-suave.js';
 import initAnimScroll from './modules/animacao-scroll.js';
 import initAccordionList from './modules/accordion.js';
 import initTabNav from './modules/tab-navigation.js';
+import initModal from './modules/modal.js';
 
 initSmoothScroll();
 initAnimScroll();
 initAccordionList();
 initTabNav();
+initModal();
