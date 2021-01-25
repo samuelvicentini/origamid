08e4dd04bcc829cdf4a91c5eb03e2ce904b0c620
Samuel Vicentini <samuelvicentini@outlook.com>
2021-01-25 19:51:02 -0300

    arquivos para aula

---
+++ JavaScript Completo ES6+/05-efeitos-no-DOM/0505-menu-dropdown/js/modules/dropDownMenu.js
@@ -0,0 +1,4 @@
+export default function initDropDownMenu() {
+  console.log('ola')
+}
+
---
+++ JavaScript Completo ES6+/05-efeitos-no-DOM/0505-menu-dropdown/js/script.js
@@ -0,0 +1,15 @@
+import initSmoothScroll from './modules/scroll-suave.js';
+import initAnimScroll from './modules/animacao-scroll.js';
+import initAccordionList from './modules/accordion.js';
+import initTabNav from './modules/tab-navigation.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropDownMenu from './modules/dropDownMenu.js';
+
+initSmoothScroll();
+initAnimScroll();
+initAccordionList();
+initTabNav();
+initModal();
+initTooltip();
+initDropDownMenu();
