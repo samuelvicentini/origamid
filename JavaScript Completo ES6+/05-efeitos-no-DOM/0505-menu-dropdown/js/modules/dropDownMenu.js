d066726f115dabf95eb01dbf49a6fd39bb0f5ce4
Samuel <samuelvicentini@outlook.com>
2021-01-28 14:16:59 -0300

    dropdown menu

--- JavaScript Completo ES6+/05-efeitos-no-DOM/0505-menu-dropdown/js/modules/dropDownMenu.js
+++ JavaScript Completo ES6+/05-efeitos-no-DOM/0505-menu-dropdown/js/modules/dropDownMenu.js
@@ -1,4 +1,19 @@
-export default function initDropDownMenu() {
-  console.log('ola')
-}
+import outsideClick from "./otsideClick.js";
 
+export default function initDropDownMenu() {
+  const dropDownMenus = document.querySelectorAll("[data-dropdown]");
+
+  dropDownMenus.forEach((menu) => {
+    ["touchstart", "click"].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+  });
+
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add("ativo");
+    outsideClick(this, ["touchstart", "click"], () => {
+      this.classList.remove("ativo");
+    });
+  }
+}
