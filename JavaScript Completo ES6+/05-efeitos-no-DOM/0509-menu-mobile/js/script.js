9ef382cac89247c84a71020dd0f1cfe7f6bfdfeb
Samuel <samuelvicentini@outlook.com>
2021-02-03 11:23:20 -0300

    criando menu do mobile

--- JavaScript Completo ES6+/05-efeitos-no-DOM/0509-menu-mobile/js/script.js
+++ JavaScript Completo ES6+/05-efeitos-no-DOM/0509-menu-mobile/js/script.js
@@ -5,6 +5,7 @@ import initTabNav from './modules/tab-navigation.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropDownMenu.js';
+import initMenuMobile from './modules/menu-mobile.js'
 
 initSmoothScroll();
 initAnimScroll();
@@ -13,3 +14,4 @@ initTabNav();
 initModal();
 initTooltip();
 initDropDownMenu();
+initMenuMobile();
